556b251da8ba2e12d43ecebc837e86ae00cd1cc7
Wildiney Di Masi <wildiney@gmail.com>
2022-09-24 11:19:39 -0300

    update bodyparser options

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app)
 
 // app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/', routes)
